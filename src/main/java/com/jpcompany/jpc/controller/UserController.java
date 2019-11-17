3a7f81bac97170002f0139c0a796716b06ed1a9f
Maurizio <mauriziobella@gmail.com>
2019-11-17 16:45:59 -0500

    mauri commit

--- src/main/java/com/jpcompany/jpc/controller/UserController.java
+++ src/main/java/com/jpcompany/jpc/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.jpcompany.jpc.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import com.jpcompany.jpc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jpcompany.jpc.model.User;
@@ -21,22 +25,45 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-//    @RequestMapping("/")
-//    public String home()
-//    {
-//        //test
-//        return "index.html";
-//        //return "home.jsp";
-//
-//    }
-//
-//    @RequestMapping("/login")
-//    public String loginPage()
-//    {
-//        return "login.html";
-//
-//    }
+    @RequestMapping("/")
+    public String home()
+    {
+        //test
+        return "index.html";
+        //return "home.jsp";
 
+    }
+
+    @RequestMapping("/login")
+    public String loginPage()
+    {
+        return "login.html";
+
+    }
+    
+    @RestController
+    public class IndexController implements ErrorController{
+
+        @RequestMapping("/error")
+        @ResponseBody
+        public String handleError(HttpServletRequest request) {
+        	Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+            Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+            return String.format("<html><body><h2>Error Page</h2><div>Status code: <b>%s</b></div>"
+                            + "<div>Exception Message: <b>%s</b></div><body></html>",
+                    statusCode, exception==null? "N/A": exception.getMessage());
+        	
+        	//return "Error handling";
+        }
+
+        @Override
+        public String getErrorPath() {
+        	return "/error";
+        }
+    }
+    
+
+    /*
     @RequestMapping(value= {"/", "/login"}, method=RequestMethod.GET)
     public ModelAndView login() {
         ModelAndView model = new ModelAndView();
@@ -44,6 +71,9 @@ public class UserController {
         model.setViewName("login.html");
         return model;
     }
+    */
+    
+    
 
 //    @RequestMapping(value= {"/register"}, method=RequestMethod.GET)
 //    public ModelAndView signup() {
@@ -75,6 +105,7 @@ public class UserController {
         return model;
     }
 
+/*
     @RequestMapping(value= {"/home/home"}, method=RequestMethod.GET)
     public ModelAndView home() {
         ModelAndView model = new ModelAndView();
@@ -85,7 +116,7 @@ public class UserController {
         model.setViewName("home/home");
         return model;
     }
-
+*/
     @RequestMapping(value= {"/access_denied"}, method=RequestMethod.GET)
     public ModelAndView accessDenied() {
         ModelAndView model = new ModelAndView();
